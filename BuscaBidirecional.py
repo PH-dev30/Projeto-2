ce7fd01014f2f52b44bbfee95b4aefd536a96d40
Pedro <pedrohsdgomes@gmail.com>
2026-09-20 17:29:25 -0300

    Coreção das aspas ('') que estavam em biblioteca, agora estão em collections

--- BuscaBidirecional.py
+++ BuscaBidirecional.py
@@ -76,6 +76,7 @@ if __name__ == "__main__":
 print("\nBusca Bidirecional: A -> I")
 caminho = busca_bidirecional(grafo_exemplo, 'A', 'I')
 print(f"Caminho encontrado: {' -> '.join(caminho)}")
+
 print("\nBusca Bidirecional: A -> H")
 caminho = busca_bidirecional(grafo_exemplo, 'A', 'H')
 print(f"Caminho encontrado: {' -> '.join(caminho)}")
